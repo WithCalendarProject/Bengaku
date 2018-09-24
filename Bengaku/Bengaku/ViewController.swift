40bc85ebf85ae4c9683b6b56dffff0cfd1b8c339
前田陸 <>
2018-09-24 20:52:08 +0900

    初期設定

---
+++ Bengaku/Bengaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bengaku
+//
+//  Created by 前田陸 on 2018/09/24.
+//  Copyright © 2018年 陸　前田. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
